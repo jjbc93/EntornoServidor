639cc14f6440cdf4ddf91d579915e96ae8108147
unknown <juanjobernabe93@gmail.com>
2016-02-03 14:00:08 +0100

    añado nuevos métodos a la clase experiencia

--- privada/Curriculum/ExperienciaModel.php
+++ privada/Curriculum/ExperienciaModel.php
@@ -33,6 +33,40 @@ class ExperienciaModel{
 		Conexion::CerrarConexion($conexion);
 		return $mensaje;
 	}
+    
+    
+    public static function eliminarExperiencia($idExperiencia){
+        $conexion = Conexion::crearConexion();
+        $sql = "DELETE FROM experiencia where id=?";
+        $query = $conexion->prepare($sql);
+        $query->bind_param("i",$idExperiencia);
+        $query->execute();
+        $query->store_result();
+        if($query->affected_rows==1){
+            $mensaje="experiencia eliminada";
+		}else{
+			$mensaje=" no se ha podido eliminar una experiencia, pruebe más tarde";
+		}
+        Conexion::cerrarConexion($conexion);
+        return $mensaje;
+    }
+    
+    public static function modificarExperiencia($datos,$idExperiencia){
+        $conexion = Conexion::crearConexion();
+        $sql = "UPDATE experiencia set empresa=?, cif=?, funciones=?, fecha_inicio=?,fecha_fin=?, ident_usuario=? WHERE id=?";
+        $query =  $conexion->prepare($sql);
+        $query->bind_param("ssssssi",$datos["empresa"],$datos["cif"],$datos["funciones"],$datos["fecha_inicio"],
+                          $datos["fecha_fin"],$datos["ident_usuario"],$idExperiencia);
+        $query->execute();
+        $query->store_result();
+        if($query->affected_rows==1){
+            $mensaje="experiencia eliminada";
+		}else{
+			$mensaje=" no se ha podido eliminar una experiencia, pruebe más tarde";
+        }
+        Conexion::cerrarConexion($conexion);
+        return $mensaje;
+    }
 }
 
 /*Incluir donse se utilice, */
